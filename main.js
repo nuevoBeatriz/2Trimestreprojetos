e0cc8b83b2a40d486cce184abfa7c07f1445850e
nuevoBeatriz <bfernandesribeiroramos@gmail.com>
2025-06-25 21:28:30 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const tempoObjetivo1 = new Date("2035-12-25T20:00:00");
+const tempoObjetivo2 = new Date("2025-12-16T22:30:00");
+const tempoObjetivo3 = new Date("2025-11-11T05:00:00");
+const tempoObjetivo4 = new Date("2025-07-28T12:00:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
